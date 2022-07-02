1ee2a54160bb2b4e1f0f3c1f52f005fd6434fe7e
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-07-01 22:36:24 -0400

    Rubocoped

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,27 +12,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_01_190725) do
+ActiveRecord::Schema[7.0].define(version: 20_220_701_190_725) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "artists", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'artists', force: :cascade do |t|
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "items", force: :cascade do |t|
-    t.bigint "artist_id", null: false
-    t.string "title"
-    t.text "description"
-    t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["artist_id"], name: "index_items_on_artist_id"
+  create_table 'items', force: :cascade do |t|
+    t.bigint 'artist_id', null: false
+    t.string 'title'
+    t.text 'description'
+    t.string 'image_url'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['artist_id'], name: 'index_items_on_artist_id'
   end
 
-  add_foreign_key "items", "artists"
+  add_foreign_key 'items', 'artists'
 end
