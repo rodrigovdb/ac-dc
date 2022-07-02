cbe9017dcb06c480aab43b8c2d884a597d7951b4
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-07-02 16:52:20 -0400

    query a single object

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,15 +6,30 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    field :items, [Types::ItemType], null: false, description: "A list of items"
-    field :artists, [Types::ArtistType], null: false, description: "A list of artists"
+    field :artists, [Types::ArtistType], description: "A list of artists"
+    field :artist,  Types::ArtistType, description: "Retrieve an artist" do
+      argument :id, ID
+    end
 
-    def items
-      Item.all
+    field :items, [Types::ItemType], description: "A list of items"
+    field :item, Types::ItemType, description: "Retrieves an item" do
+      argument :id, ID
+    end
+
+    def artist(id:)
+      Artist.find(id)
+    end
+
+    def item(id:)
+      Item.find(id)
     end
 
     def artists
       Artist.all
+    end
+
+    def items
+      Item.all
     end
   end
 end
