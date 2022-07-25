023a02163dc54420258b84e03bb8db69c1c70384
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-07-25 10:30:18 -0400

    Coverage: Ignores auto-generated graphql files

--- spec/support/simple_cov.rb
+++ spec/support/simple_cov.rb
@@ -5,6 +5,8 @@ SimpleCov.start do
   add_group 'GraphQL :: Resolvers', '/app/graphql/resolvers/'
   add_group 'GraphQL :: Mutations', '/app/graphql/mutations/'
 
+  add_filter '/app/controllers/graphql_controller.rb'
+  add_filter '/app/graphql/rails_graphql_schema.rb'
   add_filter '/spec/'
   add_filter '/config/'
 end
