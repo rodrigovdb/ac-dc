8e1e191f57b3b481e1daf18e88637c4485a22763
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-08-02 16:39:41 -0400

    apollo

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from '../interfaces/album';
-import { ALBUMS } from '../mock-albums';
+import {Apollo, gql} from 'apollo-angular';
+
+const GET_ALBUMS = gql`
+{
+  albums {
+    id
+    name
+    year
+    coverImage
+    totalDuration
+  }
+}
+`
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrls: ['./albums.component.sass']
 })
+
 export class AlbumsComponent implements OnInit {
 
-  albums = ALBUMS;
+  albums:Album[] = [];
 
   selectedAlbum?: Album;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+    this.apollo
+    .watchQuery({
+      query: GET_ALBUMS,
+    })
+    .valueChanges.subscribe((result: any) => {
+      this.albums = result.data.albums
+    });
   }
 
   onSelect(album: Album): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumsComponent } from './albums/albums.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AlbumsComponent } from './albums/albums.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
