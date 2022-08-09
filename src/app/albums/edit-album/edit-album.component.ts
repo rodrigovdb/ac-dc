ffcfecd664482c2bb5a6fe16fc25d19115615393
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-08-09 14:53:51 -0400

    Edit album

---
+++ src/app/albums/edit-album/edit-album.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Album } from 'src/app/shared';
+import { AlbumService } from '../services/album.service';
+
+@Component({
+  selector: 'app-edit-album',
+  templateUrl: '../insert-album/insert-album.component.html',
+  styleUrls: ['../insert-album/insert-album.component.scss']
+})
+export class EditAlbumComponent implements OnInit {
+  @ViewChild('formAlbum') formAlbum!: NgForm;
+  album: Album = new Album();
+
+  constructor(
+    private albumService: AlbumService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.params['id'];
+    const album = this.albumService.find(id);
+    if(album == undefined){
+      throw new Error(`Album ${id} does not exist`)
+    }
+
+    this.album = album;
+  }
+
+  save(): void {
+    if(this.formAlbum.form.valid){
+      this.albumService.update(this.album)
+      this.router.navigate(['/albums'])
+    }
+  }
+}
