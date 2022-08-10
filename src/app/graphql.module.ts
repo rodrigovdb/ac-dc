73eadff6fcb0a79be2c5bd0bc8c456459bd8c19b
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2022-08-09 21:43:43 -0400

    Apollo: Handle requests with and without headers

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -7,37 +7,35 @@ import { HttpLink } from 'apollo-angular/http';
 import { LoginService } from './auth/services/login.service';
 import { Login } from './shared';
 
-//const uri = 'https://ac-dc.herokuapp.com/graphql'; // <-- add the URL of the GraphQL server here
-const uri = 'http://localhost:3000/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'https://ac-dc.herokuapp.com/graphql'; // <-- add the URL of the GraphQL server here
 
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+function link(httpLink: HttpLink): any{
+  const service: LoginService = new LoginService()
+  const currentUser: Login = service.currentUser
+
+  if(currentUser == null) {
+    return httpLink.create({ uri })
+  }
+
   const basic = setContext((operation, context) => ({
     headers: {
       'Accept': 'charset=utf-8',
       'Content-Type': 'application/json'
     }
   }));
+
+  const auth = setContext((operation, context) => ({
+    headers: {
+      'Authorization': `Bearer ${currentUser.token}`
+    }
+  }));
   
-  const auth = setContext((operation, context) => {
-    const service: LoginService = new LoginService()
-    const currentUser: Login = service.currentUser
+  return ApolloLink.from([basic, auth, httpLink.create({ uri })])
+}
 
-    if(currentUser == null){
-      return {}
-    }
-    else{
-      return {
-        headers: {
-          'Authorization': `Bearer ${currentUser.token}`
-        }
-      }
-    }
-  })
-
-  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })])
-
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: link,
+    link: link(httpLink),
     cache: new InMemoryCache(),
   };
 }
